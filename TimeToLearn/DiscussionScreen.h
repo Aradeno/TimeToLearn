bbc3c78a34d2dcc0161151f2f01c0a3b17363e1f
nls999 <stephan-000@hotmail.com>
2014-06-25 12:48:24 +0200

    discussie scherm aangepast

--- TimeToLearn/DiscussionScreen.h
+++ TimeToLearn/DiscussionScreen.h
@@ -2,12 +2,20 @@
 //  DiscussionScreen.h
 //  TimeToLearn
 //
-//  Created by admin on 5/8/14.
+//  Created by FHICT on 25/06/14.
 //  Copyright (c) 2014 TIS. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DiscussionScreen : UITableViewController
+@interface DiscussionScreen : UIViewController<UITableViewDelegate, UITableViewDataSource>
+{
+    NSMutableArray *myArray;
+}
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property (weak, nonatomic) IBOutlet UITextField *berichtTekst;
+
+
+- (IBAction)stuurBtn:(id)sender;
 
 @end
--- TimeToLearn/DiscussionScreen.m
+++ TimeToLearn/DiscussionScreen.m
@@ -2,7 +2,7 @@
 //  DiscussionScreen.m
 //  TimeToLearn
 //
-//  Created by admin on 5/8/14.
+//  Created by FHICT on 25/06/14.
 //  Copyright (c) 2014 TIS. All rights reserved.
 //
 
@@ -14,9 +14,10 @@
 
 @implementation DiscussionScreen
 
-- (id)initWithStyle:(UITableViewStyle)style
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithStyle:style];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
@@ -26,12 +27,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+	// Do any additional setup after loading the view.
+    myArray = [NSMutableArray array];
+    [myArray addObject:@"first string"]; // same with float values
+    [myArray addObject:@"second string"];
+    [myArray addObject:@"third string"];
+    [self.myTableView reloadData];
+    self.berichtTekst.text = @"testttt";
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,81 +41,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-#pragma mark - Table view data source
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return myArray.count;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
+    UITableViewCell *cell = [self.myTableView dequeueReusableCellWithIdentifier:@"Cell"];
+    NSString *s = [myArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = s;
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (IBAction)stuurBtn:(id)sender {
+    if(self.berichtTekst !=nil){
+        //[myArray
+    }
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
